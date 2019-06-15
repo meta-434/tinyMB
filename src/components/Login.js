6a5611f14ddf22e7dccd100a958a2e7b1d78be61
meta-434 <ach1@vt.edu>
2019-06-15 18:27:07 -0400

    Intermittent Style Fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,9 @@
 import React, { Component } from "react";
 //Components
-import Dialog from "./Dialog.js";
 import AppBar from "./AppBar.js";
 import LoginForm from "./LoginForm.js";
 //material-ui
-import Paper from "@material-ui/core/Paper";
-import Container from "@material-ui/core/Container";
-import Card from "@material-ui/core/Card";
-import Button from "@material-ui/core/Button";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
+
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,13 @@
 import React, { Component } from "react";
+import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import Button from "@material-ui/core/Button";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Dialog from "./Dialog.js";
 
 class LoginForm extends Component {
   render() {
