c8dd2a6ecd06da5a93c340058b4af12dcd72304e
meta-434 <ach1@vt.edu>
2019-07-19 00:05:25 -0400

    context WIP

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import "./App.css";
 
 import Login from "./components/Login.js";
 import Home from "./components/Home.js";
-
+import AppProvider from "./components/AppProvider";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
+    console.log(this.props.context);
     return (
-      <div>
-        <Router>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-        </Router>
-      </div>
+      <AppProvider>
+        <div>
+          <Router>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+          </Router>
+        </div>
+      </AppProvider>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      empty: null
+    }
+  }
+
   render() {
     return (
       <div>
-        <p>This is the home page.</p>
+        <p>{this.props.sampleString}</p>
       </div>
     );
   }
